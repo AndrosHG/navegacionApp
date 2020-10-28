6d1f71ecdca20c3e562d1577026e80c1eb4fc593
Israel Mendoza <mbi1204@hotmail.com>
2020-10-28 10:25:07 -0600

    nvas clases

---
+++ app/src/main/java/com/sienrgitec/navegacionapp/modelos/ctRazones.java
@@ -0,0 +1,6 @@
+package com.sienrgitec.navegacionapp.modelos;
+
+import java.io.Serializable;
+
+public class ctRazones implements Serializable {
+}
---
+++ app/src/main/java/com/sienrgitec/navegacionapp/modelos/ctVehiculo.java
@@ -0,0 +1,6 @@
+package com.sienrgitec.navegacionapp.modelos;
+
+import java.io.Serializable;
+
+public class ctVehiculo implements Serializable {
+}
